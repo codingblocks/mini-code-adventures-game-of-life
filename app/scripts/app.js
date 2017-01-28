5c547c3814f9945f840dce0ba167b9972bdcf449
THEjoezack <me@joezack.com>
2017-01-28 18:50:48 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+/**
+ * scripts/app.js
+ *
+ * This is a sample CommonJS module.
+ * Take a look at http://browserify.org/ for more info
+ */
+
+'use strict';
+
+function App() {
+  console.log('app initialized');
+}
+
+module.exports = App;
+
+App.prototype.beep = function () {
+  console.log('boop');
+};
+
+var canvas = document.getElementById('maze');
+var pen = canvas.getContext('2d');
+pen.fillStyle = '#DDDDDD';
+
+var size = 3;
+
+var maze = require('amazejs');
+var m = new maze.Backtracker(canvas.width / size, canvas.height / size);
+m.generate();
+
+for(var row = 0; row < m.height; row++) {
+  for(var col = 0; col < m.width; col++) {
+    if(m.get(row, col)) {
+      pen.fillRect(col * size, row * size, size, size);
+    }
+  }
+}
