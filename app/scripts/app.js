e29b253a5d8a89728f1f0b3d5808106fd3840e2a
THEjoezack <me@joezack.com>
2017-02-14 10:11:08 -0500

    Converting the maze generator into the game of life!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,18 +19,49 @@ App.prototype.beep = function () {
 
 var canvas = document.getElementById('maze');
 var pen = canvas.getContext('2d');
-pen.fillStyle = '#DDDDDD';
+var size = 2;
+var rowCount = canvas.height / size;
+var columnCount = canvas.width / size;
 
-var size = 1;
+var generateInitialState = function (rowCount, columnCount) {
+  var maze = require('amazejs');
+  var m = new maze.Backtracker(columnCount, rowCount);
+  m.generate();
+  var matrix = [];
+  for(var row = 0; row < m.height; row++) {
+    matrix[row] = [];
+    for(var col = 0; col < m.width; col++) {
+      var state = m.get(row, col) ? 1 : 0;
+      matrix[row].push(state);
+    }
+  }
+  return matrix;
+}
 
-var maze = require('amazejs');
-var m = new maze.Backtracker(canvas.width / size, canvas.height / size);
-m.generate();
+var GameOfLife = require('game-of-life-logic');
+var gameOfLife = new GameOfLife(columnCount, rowCount)
+gameOfLife.copyMatrixAt(1, 1, generateInitialState(rowCount, columnCount));
 
-for(var row = 0; row < m.height; row++) {
-  for(var col = 0; col < m.width; col++) {
-    if(m.get(row, col)) {
+var drawGame = function(m) {
+  for(var row = 0; row < m.length; row++) {
+    for(var col = 0; col < m[row].length; col++) {
+      if(m[row][col]) {
+        pen.fillStyle = '#000000';
+      } else {
+        pen.fillStyle = '#FFFFFF';
+      }
       pen.fillRect(col * size, row * size, size, size);
     }
   }
 }
+
+var step = function() {
+  drawGame(gameOfLife.matrix);
+  gameOfLife.tick();
+}
+
+// draw the game every {intervalMs}
+var intervalMs = 500;
+setInterval(step, intervalMs);
+
+// TODO how best to stop the interval?
